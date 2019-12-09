18da17968d47eb8f888fb18bada247d0002aff7d
Tanya <tatyana.lagodich@ovcenter.com>
2019-12-09 13:05:39 +0300

    create cards

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,19 @@
 import './style/main.scss';
+
+const cards: Array<string> = ['fa-cat', 'fa-crow', 'fa-dog', 'fa-dove', 'fa-dragon', 'fa-fish'];
+
+function createCards (cards: Array<string>): void {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('cards');
+  cards.forEach(card => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    const icon = document.createElement('i');
+    icon.className = `fas ${card}`;
+    div.appendChild(icon);
+    wrapper.appendChild(div);
+  })
+  document.body.appendChild(wrapper);
+}
+
+createCards(cards);
--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: [
-          'ts-loader',
           'babel-loader',
+          'ts-loader'
         ],
         exclude: /node_modules/,
       },
@@ -36,6 +36,12 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader',
         ],
