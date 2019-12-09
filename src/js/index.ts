d57f5b08cc1ba39041138227759f047a9b42c555
Tanya <tatyana.lagodich@ovcenter.com>
2019-12-09 15:53:07 +0300

    add clickHandler

--- src/js/index.ts
+++ src/js/index.ts
@@ -3,17 +3,40 @@ import './style/main.scss';
 const cards: Array<string> = ['fa-cat', 'fa-crow', 'fa-dog', 'fa-dove', 'fa-dragon', 'fa-fish'];
 
 function createCards (cards: Array<string>): void {
-  const wrapper = document.createElement('div');
+  const wrapper: HTMLElement = document.createElement('div');
   wrapper.classList.add('cards');
   cards.forEach(card => {
-    const div = document.createElement('div');
+
+    const div: HTMLDivElement = document.createElement('div');
+    const front: HTMLDivElement = document.createElement('div');
+    const back: HTMLDivElement = document.createElement('div');
+    const icon: HTMLElement = document.createElement('i');
+
     div.classList.add('card');
-    const icon = document.createElement('i');
+    front.classList.add('front');
+    back.classList.add('back');
     icon.className = `fas ${card}`;
-    div.appendChild(icon);
-    wrapper.appendChild(div);
+
+    div.append(front, back);
+    front.append(icon);
+    wrapper.append(div);
   })
-  document.body.appendChild(wrapper);
+  document.body.append(wrapper);
+}
+
+function cliclHandler() {
+  const cards: Array<HTMLDivElement> = Array.from(document.querySelectorAll('.card'));
+  cards.forEach((card) => {
+    card.addEventListener('click', ({ target }: { target: EventTarget }) => {
+      const elem: Element = (<HTMLElement>target).classList.contains('card') ? <HTMLElement>target : (<HTMLElement>target).closest('.card');
+      if (elem) {
+        
+        elem.classList.toggle('flip');
+      }
+    })
+  })
+
 }
 
 createCards(cards);
+cliclHandler();
